172e9626cb8d290de6a50ed22d44f227109bff8f
Tanboon Tiengtut <147372532+tanboon@users.noreply.github.com>
2026-09-19 15:43:37 +0700

    chore: init project with react, typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <Container maxWidth="md">
+      <Box sx={{ my: 4 }}>Test</Box>
+    </Container>
+  );
+};
+
+export default App;
